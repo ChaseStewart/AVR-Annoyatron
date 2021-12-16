a52fc918646b142df7b864b73dcc9d9330379f4a
ChaseStewart <chst9148@colorado.edu>
2021-12-16 15:07:36 -0700

    Partial progress- USR_LED and PIR working, display NOT working.
    Transferring between computers- as mentioned the USR LED and the PIR sensor are currently working as expected, but the display is not. I'm hoping this is just a software issue.

--- XC8Application1/XC8Application1/main.c
+++ XC8Application1/XC8Application1/main.c
@@ -1,25 +1,146 @@
 /*
- * main.c
- *
- * Created: 12/11/2021 2:35:04 PM
- * Author: Chase E. Stewart for Hidden Layer Design
- */ 
+* main.c
+*
+* Created: 12/11/2021 2:35:04 PM
+* Author: Chase E. Stewart for Hidden Layer Design
+*/
+#include "main.h"
 
-#define F_CPU 3333333
 #include <xc.h>
 #include <avr/io.h>
+#define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
+
+#include "I2C.h"
+
+
+volatile uint16_t display_buffer[8];
+
 
 int main(void)
 {
-	PORTC.DIR |= PIN2_bm;
-	
-	
-    while(1)
-    {
-		PORTC.OUT |= PIN2_bm;
-		_delay_ms(500);
-		PORTC.OUT &= ~PIN2_bm;
-		_delay_ms(500);
-    }
+   initPeripherals();
+
+   ledUsrBlink(5, 500);
+   _delay_ms(1000);
+
+   initSevenSeg();
+   
+   setSevenSegValue(0, 0);
+   setSevenSegValue(1, 1);
+   setSevenSegValue(3, 2);
+   setSevenSegValue(4, 3);
+   writeSevenSeg();
+
+   while(1)
+   {
+      /* set USR_LED IFF PIR sensor is triggered */
+      PORTC.OUT = (PORTC.IN & PIN0_bm) ? PIN2_bm : 0;
+   }
 }
+
+/**
+ * Setup all peripherals 	
+ */
+void initPeripherals(void)
+{
+   initLED();
+   initPIR();
+   I2C_init();
+}
+
+void initSevenSeg(void)
+{
+   uint8_t writeCmdByte = 0;
+   uint8_t readBuf[10] = {0};
+   uint8_t status;
+
+   /* Clear keylines by reading them */
+   //I2C_read_bytes(SEVENSEG_ADDR, readBuf, HT16K33_KEYDATA0_ADDR, HT16K33_KEYDATA_LEN);
+
+   /* Enable HT16K33 oscillator */
+   writeCmdByte = 0x21; //HT16K33_CMD_OSC_ENABLE;
+   I2C_start(SEVENSEG_ADDR);
+   status =    I2C_wait_ACK();
+   if (1 == status)
+   {
+      ledUsrBlink(0, 200);
+   }
+   I2C_write(&writeCmdByte);
+   I2C_stop();
+   
+   /* Enable HT16K33 display */ 
+   writeCmdByte = 0x81; //HT16K33_CMD_DISP_ON_NOBLINK;
+   I2C_start(SEVENSEG_ADDR);
+   I2C_write(&writeCmdByte);
+   I2C_stop();
+
+   /* Set dimming level to 16/16 */
+   writeCmdByte = 0xEF; //HT16K33_CMD_DIM_LEVEL(0x0F);
+   I2C_start(SEVENSEG_ADDR);
+   I2C_write(&writeCmdByte);
+   I2C_stop();
+
+   for (int i=0; i<8; i++)
+   {
+      display_buffer[i] = 0;
+   }
+   writeSevenSeg();
+}
+
+void writeSevenSeg(void)
+{
+   uint8_t convert_array[16];
+   
+   for (int i = 0; i < 8; i++) 
+   {
+      convert_array[i << 1] = display_buffer[i] & 0xFF;
+      convert_array[(i << 1) + 1] = display_buffer[i] >> 8;
+   }
+
+   I2C_write_bytes(SEVENSEG_ADDR, convert_array, 0x00, 16);
+}
+
+void setSevenSegValue(uint8_t index, uint8_t value)
+{
+ 	/*this function sets a single position in the number table*/
+ 	if (index > 4 || value > 17) 
+   {
+    	return;
+ 	}
+ 	display_buffer[index] = numbertable[value];  
+}
+
+void initPIR(void)
+{
+   PORTC.DIRCLR = PIN0_bm;
+   PORTC.PIN0CTRL &= ~PORT_PULLUPEN_bm;
+}
+
+/** 
+ * Set up the user LED
+ */
+void initLED(void)
+{
+   PORTC.DIR |= PIN2_bm;
+}
+
+/**
+ *	Blink usr LED 'count'-many times at a period of 2 * 'blinkPeriodMsec'
+ */
+void ledUsrBlink(uint8_t count, const int blinkPeriodMsec)
+{
+   uint8_t iter=0;
+
+   while (!count || iter < count)
+   {
+       PORTC.OUT |= PIN2_bm;
+       _delay_ms(blinkPeriodMsec);
+       PORTC.OUT &= ~PIN2_bm;
+       _delay_ms(blinkPeriodMsec);
+      iter++;
+   }
+
+   // clear LED after blinking is done
+   PORTC.OUT &= ~PIN2_bm;
+}  
---
+++ XC8Application1/XC8Application1/main.h
@@ -0,0 +1,78 @@
+/*
+ * main.h
+ *
+ * Created: 12/12/2021 6:23:38 PM
+ *  Author: vtrre
+ */ 
+
+#include "stdint.h"
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define F_CPU 3333333
+
+static const uint8_t numbertable[] = {
+   0x3F, /* 0 */
+   0x06, /* 1 */
+   0x5B, /* 2 */
+   0x4F, /* 3 */
+   0x66, /* 4 */
+   0x6D, /* 5 */
+   0x7D, /* 6 */
+   0x07, /* 7 */
+   0x7F, /* 8 */
+   0x6F, /* 9 */
+   0x77, /* a */
+   0x7C, /* b */
+   0x39, /* C */
+   0x5E, /* d */
+   0x79, /* E */
+   0x71, /* F */
+   0xFF, /* all on */
+   0x00 /* all off */
+};
+
+#define TRUE 1
+#define FALSE 0
+
+#define SEVENSEG_ADDR (0x70 << 1)
+
+#define HT16K33_KEYDATA0_ADDR 0x40
+#define HT16K33_KEYDATA_LEN 5
+
+#define HT16K33_SYS_SETUP_ADDR 0x20
+#define HT16K33_SYS_SETUP_OSC_ON  0x01
+#define HT16K33_SYS_SETUP_OSC_OFF 0x00
+#define HT16K33_CMD_OSC_ENABLE (HT16K33_SYS_SETUP_ADDR | HT16K33_SYS_SETUP_OSC_ON)
+#define HT16K33_CMD_OSC_DISABLE (HT16K33_SYS_SETUP_ADDR | HT16K33_SYS_SETUP_OSC_OFF)
+
+#define HT16K33_DISP_SET_ADDR 0x80
+#define HT16K33_DISP_SET_DISPLAYON 0x01
+#define HT16K33_DISP_SET_DISPLAYOFF 0x00
+#define HT16K33_BLINK_OFF    0x00
+#define HT16K33_BLINK_2HZ    0x02
+#define HT16K33_BLINK_1HZ    0x04
+#define HT16K33_BLINK_HALFHZ 0x06
+
+#define HT16K33_DIM_ADDR 0xE0
+
+#define HT16K33_CMD_DISP_OFF (HT16K33_DISP_SET_ADDR | HT16K33_DISP_SET_DISPLAYOFF | HT16K33_BLINK_OFF)
+#define HT16K33_CMD_DISP_ON_NOBLINK (HT16K33_DISP_SET_ADDR | HT16K33_DISP_SET_DISPLAYON | HT16K33_BLINK_OFF)
+#define HT16K33_CMD_DISP_ON_BLINK (HT16K33_DISP_SET_ADDR | HT16K33_DISP_SET_DISPLAYON | HT16K33_BLINK_1HZ)
+#define HT16K33_CMD_DIM_LEVEL(dimNibble) (0xE0 + (dimNibble & 0x0F))
+
+
+void initPeripherals(void);
+void initPIR(void);
+void initLED(void);
+void initSevenSeg(void);
+
+void ledUsrBlink(uint8_t count, const int blinkPeriodMsec);
+
+void setSevenSegValue(uint8_t index, uint8_t value);
+void writeSevenSeg(void);
+
+
+
+#endif /* MAIN_H_ */
